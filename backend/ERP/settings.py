03dc3504c5f2a2f2b5262751d39de87650bdd0fa
smlnexgenllp <pugalendhi.smlnexgen@gmail.com>
2025-12-26 09:36:58 +0530

    added the tools in the settings.py by pugal

--- backend/ERP/settings.py
+++ backend/ERP/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'simple_history',
     
     # Your custom apps
     'apps.accounts',
@@ -71,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'ERP.urls'
@@ -190,4 +192,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # settings.py or local.py
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+SESSION_COOKIE_HTTPONLY = False
+SESSION_COOKIE_SAMESITE = 'Lax'
+SESSION_COOKIE_SECURE = False  # True in prod
 
+CSRF_COOKIE_HTTPONLY = False
+CSRF_COOKIE_SAMESITE = 'Lax'
+CSRF_COOKIE_SECURE = False
