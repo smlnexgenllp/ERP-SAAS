20d1bbc407b3a2ac0409626e0cfd7ee14321bbe7
smlnexgenllp <pugalendhi.smlnexgen@gmail.com>
2026-01-13 11:30:36 +0530

    created the urls for vendors

--- backend/apps/finance/urls.py
+++ backend/apps/finance/urls.py
@@ -3,11 +3,14 @@ from rest_framework.routers import DefaultRouter
 
 from apps.finance.views.budget import MonthlyBudgetViewSet
 from apps.finance.views.department_budget import DepartmentBudgetViewSet
+from apps.finance.views.vendor import VendorViewSet
 
 router = DefaultRouter()
 router.register("monthly-budgets", MonthlyBudgetViewSet, basename="monthly-budget")
 router.register("department-budgets", DepartmentBudgetViewSet, basename="department-budget")
+router.register(r"vendors", VendorViewSet, basename="vendor")
 
 urlpatterns = [
     path("", include(router.urls)),
+   
 ]
