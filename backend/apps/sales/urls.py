7b390e76b5ec8f85b6355f0f52743b0857364fb7
smlnexgenllp <pugalendhi.smlnexgen@gmail.com>
2026-03-06 16:58:31 +0530

    created the urls

---
+++ backend/apps/sales/urls.py
@@ -0,0 +1,12 @@
+# sales/urls.py
+from django.urls import path
+from .views import (
+    QualifiedLeadsListView,
+    QualifiedLeadDetailView,
+    CreateSalesLeadView,
+)
+
+urlpatterns = [
+    path('qualified-leads/', QualifiedLeadsListView.as_view(), name='qualified-leads-list'),
+    path('leads/<int:id>/', QualifiedLeadDetailView.as_view(), name='lead-detail'),
+]
